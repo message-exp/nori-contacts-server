bb3104baf465bd905f298c7721ac505381db4144
ErEr <jeffliao92316046@gmail.com>
2025-07-11 02:19:21 +0800

    feat: implement lifespan management for FastAPI and update root endpoint response

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
-from typing import Union
-
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from api.router import api_router
+from core.config import settings
+from db.session import init_db_pool
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db_pool()
+    yield
+
+
+app = FastAPI(
+    title="nori contacts server",
+    version="0.1.0",
+    description="A robust backend API using FastAPI.",
+    docs_url="/docs" if settings.APP_MODE != "prod" else None,
+    redoc_url="/redoc" if settings.APP_MODE != "prod" else None,
+    lifespan=lifespan,
+)
+
+app.include_router(api_router, prefix="/api")
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"message": "Welcome to the nori-contacts-server!"}
