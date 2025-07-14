4d56ff70a140fcdb4902fbb68ef70d8dbaf796db
ErEr <jeffliao92316046@gmail.com>
2025-07-14 19:13:16 +0800

    feat: change contact_card_id type from int to UUID in PlatformContactResponse and PlatformContactCreate models

--- src/schemas/platform_contact.py
+++ src/schemas/platform_contact.py
@@ -1,8 +1,19 @@
+from uuid import UUID
 from pydantic import BaseModel
 from models import PlatformEnum
 
 
 class PlatformContactResponse(BaseModel):
-    contact_card_id: int
+    contact_card_id: UUID
     platform: PlatformEnum
+    platform_user_id: str
+
+
+class PlatformContactCreate(BaseModel):
+    contact_card_id: UUID
+    platform: PlatformEnum
+    platform_user_id: str
+
+
+class PlatformContactUpdate(BaseModel):
     platform_user_id: str
