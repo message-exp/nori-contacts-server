dd5a03f828f76e0a75b821c93590f1299c80cc37
ErEr <jeffliao92316046@gmail.com>
2025-07-14 15:32:48 +0800

    feat: add update and delete functionalities in contact card servicer

--- src/services/contact_card_service.py
+++ src/services/contact_card_service.py
@@ -1,8 +1,12 @@
 import asyncpg
-from schemas.contact_card import ContactCardCreate
+from fastapi import HTTPException, status
+from schemas.contact_card import ContactCardCreate, ContactCardDelete, ContactCardUpdate
 from db.repositories.contact_cards_repository import (
     get_contact_cards_by_owner,
     insert_contact_card,
+    update_contact_card as update_contact_card_in_db,
+    delete_contact_card as delete_contact_card_in_db,
+    get_single_contact_card_by_id_and_owner,
 )
 
 
@@ -13,6 +17,44 @@ class ContactCardServicer:
     async def create_contact_card(
         self, db: asyncpg.Connection, user_id: str, contact: ContactCardCreate
     ):
-        await insert_contact_card(
-            db, user_id, contact.contact_name,contact.nickname ,contact.contact_avatar_url
+        return await insert_contact_card(
+            db,
+            user_id,
+            contact.contact_name,
+            contact.nickname,
+            contact.contact_avatar_url,
+        )
+
+    async def update_contact_card(
+        self, db: asyncpg.Connection, user_id: str, contact: ContactCardUpdate
+    ):
+        record = await get_single_contact_card_by_id_and_owner(db, contact.id, user_id)
+        if not record:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Contact card not found."
+            )
+        updated_contact_card = await update_contact_card_in_db(
+            db,
+            contact.id,
+            contact.contact_name,
+            contact.nickname,
+            contact.contact_avatar_url,
+        )
+        if updated_contact_card is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Contact card not found."
+            )
+        return updated_contact_card
+
+    async def delete_contact_card(
+        self, db: asyncpg.Connection, user_id: str, contact: ContactCardDelete
+    ):
+        record = await get_single_contact_card_by_id_and_owner(db, contact.id, user_id)
+        if not record:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Contact card not found."
+            )
+        await delete_contact_card_in_db(
+            db,
+            contact.id,
         )
