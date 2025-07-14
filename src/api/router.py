ed7320708499b51aadd7f1aa1b94bd6582b3f99d
ErEr <jeffliao92316046@gmail.com>
2025-07-14 19:14:16 +0800

    feat: include platform contact router in API

--- src/api/router.py
+++ src/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from api.routers import contact_card
+from api.routers import contact_card, platform_contact
 
 api_router = APIRouter()
 api_router.include_router(contact_card.router, prefix="/contact-cards")
+api_router.include_router(platform_contact.router, prefix="/platform-contacts")
