90b7cd7ad6317498bc2b6590e52820a6d2adef1b
Barney <barney9211@gmail.com>
2025-08-13 17:09:25 +0000

    Update: add HTTPException

--- src/api/routers/bridges/bridge_discord.py
+++ src/api/routers/bridges/bridge_discord.py
@@ -10,16 +10,24 @@ router = APIRouter(tags = ["Mautrix Discord"])
 @router.get("/login/qrcode" , response_model = LoginWithQrcodeResponse)
 async def login_with_qr(user_id: str = Depends(get_user_id_from_header)):
     data , status= await bridge_discord_service.login_with_qr(user_id)
+    if status is not 200:
+        raise HTTPException(status_code=status, detail=data)
     return data
 @router.post("/logout", response_model = LogoutResponse)
 async def logout(user_id: str = Depends(get_user_id_from_header)):
     data , status = await bridge_discord_service.logout(user_id)
+    if status is not 200:
+        raise HTTPException(status_code=status, detail=data)
     return data
 @router.get("/ping" , response_model = PingResponse)
 async def ping(user_id : str = Depends(get_user_id_from_header)):
     data , status = await bridge_discord_service.ping(user_id)
+    if status is not 200:
+        raise HTTPException(status_code=status, detail=data)
     return data
 @router.post("/login/token" , response_model = LoginWithTokenResponse)
 async def login_with_token(loginWithTokenRequest : LoginWithTokenRequest , user_id: str = Depends(get_user_id_from_header)):
     data , status = await bridge_discord_service.login_with_token(loginWithTokenRequest.token , user_id , loginWithTokenRequest.token_type)
+    if status is not 200:
+        raise HTTPException(status_code=status, detail=data)
     return data
